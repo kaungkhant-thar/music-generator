48ea6937582b722591dfa2f46ef4bd7c129e4963
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2025-08-09 01:24:50 +0700

    feat: added custom lyrics generation

--- backend/main.py
+++ backend/main.py
@@ -207,7 +207,7 @@ class MusicGenServer:
             s3_key=audio_s3_key, cover_image_s3_key=image_s3_key, categories=categories
         )
 
-    @modal.fastapi_endpoint(method="POST")
+    @modal.fastapi_endpoint(method="POST", requires_proxy_auth=True)
     def generate_from_description(
         self, request: GenerateFromDescriptionRequest
     ) -> GenerateMusicResponseS3:
@@ -226,27 +226,44 @@ class MusicGenServer:
             **request.model_dump(exclude={"full_described_song"}),
         )
 
-    @modal.fastapi_endpoint(method="POST")
-    def generate_with_lyrics(self) -> GenerateMusicResponseS3:
-        pass
+    @modal.fastapi_endpoint(method="POST", requires_proxy_auth=True)
+    def generate_with_lyrics(
+        self, request: GenerateWithCustomLyricsRequest
+    ) -> GenerateMusicResponseS3:
+        return self.generate_and_upload_to_s3(
+            description_for_categorization=request.lyrics,
+            **request.model_dump(exclude={"full_described_song"}),
+        )
 
-    @modal.fastapi_endpoint(method="POST")
-    def generate_with_described_lyrics(self) -> GenerateMusicResponseS3:
+    @modal.fastapi_endpoint(method="POST", requires_proxy_auth=True)
+    def generate_with_described_lyrics(
+        self, request: GenerateWithDescribedLyricsRequest
+    ) -> GenerateMusicResponseS3:
+
         # generate lyrics
-        pass
+        lyrics = ""
+        if not request.instrumental:
+            lyrics = self.generate_lyrics(request.described_lyrics)
+
+        return self.generate_and_upload_to_s3(
+            lyrics=lyrics,
+            description_for_categorization=request.described_lyrics,
+            **request.model_dump(exclude={"described_lyrics"}),
+        )
 
 
 @app.local_entrypoint()
 def main():
     server = MusicGenServer()
-    endpoint_url = server.generate_from_description.get_web_url()
+    endpoint_url = server.generate_with_described_lyrics.get_web_url()
 
-    request_data = GenerateFromDescriptionRequest(
-        full_described_song="Write a Upbeat Country song"
+    request_data = GenerateWithDescribedLyricsRequest(
+        prompt="rock, drum, bass",
+        described_lyrics="a story about success",
     )
     payload = request_data.model_dump()
 
-    response = requests.post(endpoint_url, json=payload)
+    response = requests.post(endpoint_url, json=payload, headers=headers)
     response.raise_for_status()
 
     result = GenerateMusicResponseS3(**response.json())
