068f903aee0d13c28963ac822793584314c0080a
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2025-08-09 11:55:59 +0700

    feat: added auth

---
+++ frontend/src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@daveyplate/better-auth-ui";
+import React from "react";
+
+export default async function Dashboard() {
+  return (
+    <div>
+      Dashboard
+      <UserButton size="icon" />
+    </div>
+  );
+}
---
+++ frontend/src/app/(protected)/layout.tsx
@@ -0,0 +1,16 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+const ProtectedLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  console.log({ session });
+  if (!session) {
+    redirect("/auth/sign-in");
+  }
+  return <div>{children}</div>;
+};
+
+export default ProtectedLayout;
